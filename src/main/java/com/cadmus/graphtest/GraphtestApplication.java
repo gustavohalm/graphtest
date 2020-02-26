22edb7be376829948a31736b44aed97868f05ad1
Gustavo P. Almeida <gustavo.almeida@cadmus.com.br>
2020-02-26 10:33:23 -0300

    initial commit

---
+++ src/main/java/com/cadmus/graphtest/GraphtestApplication.java
@@ -0,0 +1,13 @@
+package com.cadmus.graphtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cadmus/graphtest/GraphtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cadmus.graphtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
